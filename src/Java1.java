3c2ca0ab5cea041f289203c3ff2574455d04c0d6
shubhamkfa <shubhamkfa@gmail.com>
2023-12-28 12:27:27 +0530

    variable 28/12/2023

--- src/Java1.java
+++ src/Java1.java
@@ -1,6 +1,6 @@
 public class Java1 {
     public static void main(String[] args) {
-//        Variablesas
+//        Variables as on 28/12/2023
         int a = 10;
         int b = 5;
         int ans = (a * b) / (a - b);
