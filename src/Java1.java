42caca5ab001fe3424614b581a6a673fbd6a411e
shubhamkfa <shubhamkfa@gmail.com>
2023-12-28 12:25:05 +0530

    variable

--- src/Java1.java
+++ src/Java1.java
@@ -1,10 +1,11 @@
 public class Java1 {
     public static void main(String[] args) {
-//        Variables
+//        Variablesas
         int a = 10;
         int b = 5;
         int ans = (a * b) / (a - b);
         System.out.println(ans);
+
 
 
     }
